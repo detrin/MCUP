762cf010d4a8fdd55faf59dfaf01728a037188d7
Daniel Herman <daniel.herman@protonmail.com>
2026-04-03 13:14:30 +0200

    feat: add WeightedRegressor with analytical and MC methods

---
+++ mcup/weighted.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+from .base import BaseRegressor
+from ._analytical import analytical_solve
+from ._mc import mc_solve
+
+
+class WeightedRegressor(BaseRegressor):
+    def fit(self, X, y, y_err, p0):
+        X, y, y_err = self._validate_inputs(X, y, y_err)
+        p0 = np.asarray(p0, dtype=float)
+        self._y_err_fit_ = y_err
+
+        weights = 1.0 / (y_err ** 2)
+
+        if self.method == "analytical":
+            params, cov = analytical_solve(self.func, X, y, weights, p0, self.optimizer)
+            self.params_ = params
+            self.covariance_ = cov
+            self.params_std_ = np.sqrt(np.diag(cov))
+        else:
+            def cost_fn_builder(x_s, y_s, params_est):
+                w = 1.0 / (y_err ** 2)
+
+                def cost(params):
+                    r = np.array([y_s[i] - self.func(x_s[i], params) for i in range(len(y_s))])
+                    return float(np.dot(r ** 2, w))
+
+                return cost
+
+            mean, cov, n = mc_solve(
+                cost_fn_builder, X, y, None, y_err, p0,
+                self.n_iter, self.rtol, self.atol, self.optimizer,
+            )
+            self.params_ = mean
+            self.covariance_ = cov
+            self.params_std_ = np.sqrt(np.diag(cov))
+            self.n_iter_ = n
+
+        return self
